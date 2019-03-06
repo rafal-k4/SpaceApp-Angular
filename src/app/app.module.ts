6ff8751bb59a32cd8ea90905384a5657cda7399b
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2019-03-06 20:07:06 +0100

    task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { A2sCommModule } from 'a2s-comm';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    A2sCommModule
   ],
   providers: [],
   bootstrap: [AppComponent]
