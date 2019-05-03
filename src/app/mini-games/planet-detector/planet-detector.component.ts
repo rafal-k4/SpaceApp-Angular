1201a797741ec411d6fa5669021ca8a76e01c748
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2019-05-03 23:40:40 +0200

    work in progress

--- src/app/mini-games/planet-detector/planet-detector.component.ts
+++ src/app/mini-games/planet-detector/planet-detector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-planet-detector',
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class PlanetDetectorComponent implements OnInit {
 
   constructor() { }
+
+  @HostListener('mousemove') update() {
+    console.log()
+  }
+
 
   ngOnInit() {
   }
