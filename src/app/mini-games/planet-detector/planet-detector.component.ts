e367585156cfb248fd237dc0ff053d579b451431
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2019-05-05 00:02:19 +0200

    calculating distance from planet

--- src/app/mini-games/planet-detector/planet-detector.component.ts
+++ src/app/mini-games/planet-detector/planet-detector.component.ts
@@ -7,32 +7,35 @@ import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/co
 })
 export class PlanetDetectorComponent implements OnInit {
 
+  private dimensions;
+  private distanceFromPlanet: number;
+  private planetCoordinates: { x: number, y: number };
+
+  @ViewChild('detectionArea') detectionAreaElement: ElementRef;
+  @ViewChild('planet') planetElement: ElementRef;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
-  private dimensions;
-  @ViewChild('detectionArea') detectionAreaElement: ElementRef;
-
-  onMouseMove(position: {x: number, y: number}): void{
-    console.log(this.detectionAreaElement);
+  onMouseMove(position: { x: number, y: number }): void {
+    this.distanceFromPlanet = this.CalculateDistanceFromPlanet(position, this.planetCoordinates);
+    console.log(this.distanceFromPlanet);
   }
 
+  CalculateDistanceFromPlanet(mousePosition: { x: number, y: number }, planetPosition: { x: number, y: number }): number {
+    const verticalDist = planetPosition.x - mousePosition.x;
+    const horizontalDist = planetPosition.y - mousePosition.y;
+    return Math.sqrt(Math.pow(verticalDist, 2) + Math.pow(horizontalDist, 2));
+  }
 
   ngOnInit() {
-
     this.dimensions = this.detectionAreaElement.nativeElement.getBoundingClientRect();
-    console.log(this.dimensions, this.dimensions.width, this.dimensions.height);
+    this.planetCoordinates = { x: this.dimensions.width * Math.random(), y: this.dimensions.height * Math.random() };
+    // TODO: make sure planet isnt spawning over the border
+    console.log(this.planetCoordinates);
 
-    const div = this.renderer.createElement('div');
-    this.renderer.setStyle(div, 'height', '100px');
-    this.renderer.setStyle(div, 'width', '100px');
-    this.renderer.setStyle(div, 'background', 'white');
-    this.renderer.setStyle(div, 'position', 'absolute');
+    this.renderer.setStyle(this.planetElement.nativeElement, 'top', `${this.planetCoordinates.y}px`);
+    this.renderer.setStyle(this.planetElement.nativeElement, 'left', `${this.planetCoordinates.x}px`);
 
-    this.renderer.setStyle(div, 'top', `${this.dimensions.height * 0.5}px`);
-    this.renderer.setStyle(div, 'left', `${this.dimensions.width * 0.5}px`);
-
-    this.renderer.appendChild(this.detectionAreaElement.nativeElement, div);
   }
 
 }
