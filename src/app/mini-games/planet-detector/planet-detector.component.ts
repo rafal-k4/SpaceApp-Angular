490201e5f6c4caef07baed2f13b18a6a74f3aef3
rafal-k4 <42941375+rafal-k4@users.noreply.github.com>
2019-05-04 23:25:14 +0200

    creating element in detection area

--- src/app/mini-games/planet-detector/planet-detector.component.ts
+++ src/app/mini-games/planet-detector/planet-detector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-planet-detector',
@@ -7,15 +7,32 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class PlanetDetectorComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  private dimensions;
+  @ViewChild('detectionArea') detectionAreaElement: ElementRef;
 
   onMouseMove(position: {x: number, y: number}): void{
-    console.log(`${position.x} ${position.y}`);
+    console.log(this.detectionAreaElement);
   }
 
 
   ngOnInit() {
+
+    this.dimensions = this.detectionAreaElement.nativeElement.getBoundingClientRect();
+    console.log(this.dimensions, this.dimensions.width, this.dimensions.height);
+
+    const div = this.renderer.createElement('div');
+    this.renderer.setStyle(div, 'height', '100px');
+    this.renderer.setStyle(div, 'width', '100px');
+    this.renderer.setStyle(div, 'background', 'white');
+    this.renderer.setStyle(div, 'position', 'absolute');
+
+    this.renderer.setStyle(div, 'top', `${this.dimensions.height * 0.5}px`);
+    this.renderer.setStyle(div, 'left', `${this.dimensions.width * 0.5}px`);
+
+    this.renderer.appendChild(this.detectionAreaElement.nativeElement, div);
   }
 
 }
